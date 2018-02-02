a0c84134c656b2989ec547dc13d5feb5fb369cb8
Sushanth Suresh <sushanthkengunte@gmail.com>
2018-02-02 03:49:08 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine","ejs");
+
+
+app.get("/",function(req,res){
+   res.render("landing");
+   
+});
+
+app.get("/campgrounds",function(req,res){
+    
+    var campGrounds = [
+            
+            {
+                name:"Rishikesh",
+                url: "https://upload.wikimedia.org/wikipedia/commons/c/c9/A_camp_site_by_the_Ganga%2C_Rishikesh.jpg"
+            },
+            
+               {
+                name:"Spiti Valley",
+                url: "http://www.spitiholidayadventure.com/storage/5970aa504f376.jpg"
+            },
+        
+            {
+                name:"Jaisalmer, Rajasthan",
+                url: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX6429232.jpg"
+            },
+           {
+                name:"Chandertal Lake – Himachal Pradesh",
+                url: "https://media-cdn.tripadvisor.com/media/photo-s/09/96/2a/90/chandertal-lake.jpg"
+            },
+           {
+                name:"Manali Solang Valley – Himachal Pradesh.",
+                url: "https://www.tourmyindia.com/images/solang-valley4.jpg"
+            },
+        
+           {
+                name:"Mussoorie, Uttarakhand",
+                url: "http://eragenx.com/wp-content/uploads/2016/08/Mussoorie.jpg"
+            },
+        
+           {
+                name:"Pushkar, Rajasthan",
+                url: "http://4.bp.blogspot.com/-JE4ElFpW3k8/UmJeAzR--OI/AAAAAAAAD4I/rSlXap2MhoA/s1600/rajasthan-pushkar_lake.jpg"
+            },
+           {
+                name:"Sonamarg, Jammu and Kashmir",
+                url: "http://www.indialine.com/travel/images/sonmarg.jpg"
+            },
+           {
+                name:"Kaudiyala – Uttaranchal",
+                url: "http://www.indiawaterportal.org/sites/indiawaterportal.org/files/styles/node_lead_image_new/public/6172023818_988b81c090_b_2.jpg?itok=oYQXiRlA"
+            }
+        
+        
+        
+        ];
+    res.render("campgrounds",{campGrounds:campGrounds});
+    
+});
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    
+   console.log("Start Camping!!"); 
+});
